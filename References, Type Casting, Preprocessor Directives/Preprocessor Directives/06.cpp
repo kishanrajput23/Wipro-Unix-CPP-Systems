548e8ddc308f3ed0d2609f469b4e6913c8a0323a
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2023-01-23 19:34:32 +0530

    References, Type Casting, Preprocessor Directives

--- References, Type Casting, Preprocessor Directives/Preprocessor Directives/06.cpp
+++ References, Type Casting, Preprocessor Directives/Preprocessor Directives/06.cpp
@@ -6,29 +6,27 @@ using namespace std;
 #if num > 200
    #undef num
    #define num 200
+   
 #elif num < 50
    #undef num
    #define num 50
+   
 #else
    #undef num
    #define num 100
+   
 #endif
   
-int main()
-{
+int main() {
     cout << num<<endl;
-    if(num==200)
-    {
+    if(num==200) {
         cout<<"num changed in #if block";
     }
-    else if (num==50)
-    {
+    else if (num==50) {
         cout<<"num changed in #elif block";
     }
-    else
-    {
+    else {
         cout<<"num changed in #else block";
     }
-    
-    
+    return 0;
 }
