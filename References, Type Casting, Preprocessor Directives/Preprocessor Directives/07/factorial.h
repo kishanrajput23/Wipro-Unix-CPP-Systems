d397c6e1fba681c3cfcae8a989e07a261b699550
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2023-01-23 19:44:51 +0530

    References, Type Casting, Preprocessor Directives

--- References, Type Casting, Preprocessor Directives/Preprocessor Directives/07/factorial.h
+++ References, Type Casting, Preprocessor Directives/Preprocessor Directives/07/factorial.h
@@ -1,10 +1,7 @@
-int factorial(int number)
-{
+int factorial(int number) {
   int i, fact=1;
-  for(i=1; i<=number; i++)
-  {
-      fact=fact*i;
+  for(i=1; i<=number; i++) {
+      fact *=i;
   }
   return factorial;
 }
-
