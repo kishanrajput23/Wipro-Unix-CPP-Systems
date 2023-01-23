c4db5cf995af4081b700a13daab5be0168db3bfe
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2023-01-23 19:32:59 +0530

    References, Type Casting, Preprocessor Directives

--- References, Type Casting, Preprocessor Directives/Preprocessor Directives/05.cpp
+++ References, Type Casting, Preprocessor Directives/Preprocessor Directives/05.cpp
@@ -2,22 +2,21 @@
 using std::cin;
 using std::cout;
 using std::endl;
+
 #define DEBUG
-void add()
-{
-    #ifdef DEBUG
-    {
-        cout<<"DEBUG PRINT - "<<__FUNCTION__<<" and line no. "<<__LINE__<<endl;
-    }
-    #endif 
-}
-int main()
-{
-    add();
-    #ifdef DEBUG
-    {
+
+void add() {
+    #ifdef DEBUG {
         cout<<"DEBUG PRINT - "<<__FUNCTION__<<" and line no. "<<__LINE__<<endl;
     }
     #endif 
 }
 
+int main() {
+    add();
+    
+    #ifdef DEBUG {
+        cout<<"DEBUG PRINT - "<<__FUNCTION__<<" and line no. "<<__LINE__<<endl;
+    }
+    #endif 
+}
