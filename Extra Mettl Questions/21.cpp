dfd7bc855fb07faa8f5897219ab573bc65ef7cb0
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2023-02-09 17:06:57 +0530

    xtra Mettl Questions

---
+++ Extra Mettl Questions/21.cpp
@@ -0,0 +1,19 @@
+bool isPrime(int n) {
+    for (int i=2; i<n; i++) {
+        if (n%i==0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int countPrimesInRange(int input1, int input2) {
+    int count = 0;
+    for (int i=input1; i<=input2; i++) {
+        if (isPrime(i)) {
+            count+=1;
+        }
+    }
+    return count;
+    throw "Function countPrimesInRange(int input1,int input2) not implemented. ";
+}
