875afe7a5e4e68f8e19ab37b015b9571cd6bbbf1
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2023-02-17 14:34:45 +0530

    Renamng

--- Logic Hour Building/Day_06/02.cpp
+++
@@ -1,9 +0,0 @@
-int nFactorial(int input1) {
-    if (input1==0 || input1==1) {
-        return 1;
-    }
-    else {
-        return input1 * nFactorial(input1-1);
-    }
-    throw "Function nFactorial(int input1) not implemented. ";
-}
