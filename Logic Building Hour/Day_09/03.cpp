58757f204d39d1800ae4e6ac647a34bb24052eac
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2023-07-27 17:26:55 +0530

    Logic Building Hour

--- Logic Building Hour/Day_09/03.cpp
+++ Logic Building Hour/Day_09/03.cpp
@@ -1,41 +1,21 @@
-#include<alogorithm>
+#include<algorithm>
 using namespace std;
+
 int createPIN(int input1, int input2, int input3) {
 	// Write code and remove the below exception.
-    int arr[3] = {input1, input2, input3};
-    int a[9], k=0;
-    
-    for (int i=0; i<3; i++) {
-        int x = arr[i];
-        while(x>0) {
-            int rem = x%10;
-            a[k] = rem; 
-            k++;
-            x/=10;
-        }
-    }
-    int ones[3];
-    int tens[3];
-    int hund[3];
-    
-    ones[0] = input1%10;
-    ones[1] = input2%10;
-    ones[2] = input3%10;
-
-    tens[0] = (input1/10)%10;
-    tens[1] = (input2/10)%10;
-    tens[2] = (input3/10)%10;
-
-    hund[0] = (input1/100)%10;
-    hund[1] = (input2/100)%10;
-    hund[2] = (input3/100)%10;
-
+    int ones[3] = {input1%10, input2%10, input3%10};
     sort(ones, ones+3);
-    sort(tens, tens+3);
-    sort(hund, hund+3);
-    sort(a, a+9);
 
-    int num = a[8] * 1000 + hund[0] *100 + tens[0] * 10 + ones[0] * 1;
+    int tens[3] = {(input1/10)%10, (input2/10)%10, (input3/10)%10};
+    sort(tens, tens+3);
+
+    int hun[3] = {(input1/100)%10, (input2/100)%10, (input3/100)%10};
+    sort(hun, hun+3);
+
+    int thos[3] = {ones[2], tens[2], hun[2]};
+    sort(thos, thos+3);
+
+    int num = thos[2]*1000 + hun[0]*100 + tens[0]*10 + ones[0];
     return num;
 
     throw "Function createPIN(int input1,int input2,int input3) not implemented. ";
